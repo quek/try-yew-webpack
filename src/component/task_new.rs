ea62eba2e842bb974d139b905bd8ee34cdd1efce
TAHARA Yoshinori <read.eval.print@gmail.com>
2019-07-23 20:36:24 +0900

    FormData 使うのどうなんだ？

--- src/component/task_new.rs
+++ src/component/task_new.rs
@@ -1,9 +1,12 @@
 use yew::{html, Component, ComponentLink, Html, Renderable, ShouldRender};
+use stdweb::web::FormData;
+use stdweb::traits::IEvent;
+use stdweb::unstable::TryInto;
 
 pub struct Model {}
 
 pub enum Msg {
-    Submit,
+    Submit(stdweb::web::event::SubmitEvent),
 }
 
 impl Component for Model {
@@ -16,8 +19,11 @@ impl Component for Model {
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::Submit => {
-                console!(log, "submit");
+            Msg::Submit(event) => {
+                event.prevent_default();
+                let form: stdweb::web::Element = event.target().unwrap().try_into().unwrap();
+                let form_data = FormData::from_element(&form).unwrap();
+                form_data.get("name");
             }
         }
         true
@@ -32,8 +38,10 @@ impl Renderable<Model> for Model {
     fn view(&self) -> Html<Self> {
         html! {
             <div class="TaskNew", >
-                <input type="text", placeholder="内容", />
-                <button>{"OK"}</button>
+                <form onsubmit=|event| Msg::Submit(event), >
+                    <input name="name", type="text", placeholder="内容", />
+                    <button>{"OK"}</button>
+                </form>
             </div>
         }
     }
